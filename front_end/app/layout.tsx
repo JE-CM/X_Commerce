437f98ab1516c106dce629726ad4a3ba82c47502
JE-CM <120199593+JE-CM@users.noreply.github.com>
2024-10-29 00:33:53 +0000

    First commit, Front.1

--- front_end/app/layout.tsx
+++ front_end/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/app/ui_ux/global.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./ui_ux/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./ui_ux/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
